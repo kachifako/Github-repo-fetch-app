2fc817f24fa7652991c803e0dec014d800ecfeed
Onyekachi Fakoya <88961671+kachifako@users.noreply.github.com>
2022-11-06 21:45:01 +0100

    deployment corrections

--- src/pages/userProfile/UserProfile.js
+++ src/pages/userProfile/UserProfile.js
@@ -29,33 +29,35 @@ export default function UserProfile(id, name, html_url, avatar_url) {
   }, []);
 
   return (
-
     <article className="profile-body">
-       {loading ? (<h2>Loading...</h2>) :
-        
-      (<><h1 className="profile-h1">{user.name}'s Github Profile</h1>
-      <div className="about-user">
-        <img
-          className="profile-pic"
-          alt="kachifako's picture"
-          src={user.avatar_url}
-        />
-        <div className="user-details">
-          <h2 className="profile-h2"> Username: {user.login}</h2>
-          <a className="profile-link" href={user.html_url}>
-            See more about me <span>here</span>.
-          </a>
-          <br />
-          <a
-            className="profile-link"
-            target="_blank"
-            href="https://twitter.com/pwettykach"
-          >
-            Follow me on <span>Twitter</span>
-          </a>
-        </div>
-      </div></>)}
-        
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h1 className="profile-h1">{user.name}'s Github Profile</h1>
+          <div className="about-user">
+            <img
+              className="profile-pic"
+              alt="kachifako"
+              src={user.avatar_url}
+            />
+            <div className="user-details">
+              <h2 className="profile-h2"> Username: {user.login}</h2>
+              <a className="profile-link" href={user.html_url}>
+                See more about me <span>here</span>.
+              </a>
+              <br />
+              <a
+                className="profile-link"
+                target="_blank" rel="noreferrer"
+                href="https://twitter.com/pwettykach"
+              >
+                Follow me on <span>Twitter</span>
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </article>
   );
 }
